06d801dad6a5c927071a6c869ca3b9cdf60b4592
jonhindsgaul <jon.hindsgaul@gmail.com>
2020-04-14 19:07:49 +0200

    adding mongoDB to dhtreading_service.py. Still missing data post, herein data construction

--- dhtreading_service.py
+++ dhtreading_service.py
@@ -3,8 +3,10 @@ import time
 from datetime import datetime
 import os
 
+import pymongo
+from pymongo import MongoClient
+
 # Adafruit_DHT library to read the sensor
-# 
 import Adafruit_DHT
 
 #Assuming the database is at the user level
@@ -13,6 +15,10 @@ os.chdir('/home/pi/database/')
 #Sensor type and Raspberry Pi Zero W GPIO pin in use
 sensor = 11
 pin = 4
+
+#MongoDB access
+client = MongoClient('mongodb+srv://pi:Jnhjnh22@mongodbatlascluster-xpvxg.mongodb.net/test?retryWrites=true&w=majority')
+db = client.dhtreadings
 
 #Reading the sensor - sometimes the reading is troublesome, because DHT11 sensors can be difficult to read
 def dht_read():
@@ -28,13 +34,24 @@ def dht_read():
         append_copy.write(original_text)
         append_copy.close()
         print("Recording succeeded")
+        return humidity, temperature
     except:
         print("Failed to read, trying again")
         time.sleep(1)
         dht_read()
 
+#Posting to MongoDB
+def mongodb_post(humidity, temperature):
+
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+
+
+
 #Making the script run
 while True:
-    dht_read()
+    humidity, temperature = dht_read()
+    mongodb_post(humidity, temperature)
     time.sleep(900.0)
 
